1d229f78a53b28384a067a8be1d438c8bbfd71c8
brittwoodard <96916819+brittwoodard@users.noreply.github.com>
2023-07-18 12:49:48 +0200

    hover container

--- main.js
+++ main.js
@@ -1,10 +1,1 @@
-const hoverDiv = document.querySelector('.hover-div');
-  const targetDiv = document.querySelector('.target-div');
-
-  hoverDiv.addEventListener('mouseover', () => {
-    targetDiv.style.display = 'block';
-  });
-
-  hoverDiv.addEventListener('mouseout', () => {
-    targetDiv.style.display = 'none';
-  });
+import 'alpinejs';
