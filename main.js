b82b07eb3e58346d0f9e6d7fd4dd6933810dc3c1
brittwoodard <96916819+brittwoodard@users.noreply.github.com>
2023-07-18 10:40:58 +0200

    contact form

--- main.js
+++ main.js
@@ -1,34 +1,10 @@
-// Get the popup container element
-const popupContainer = document.getElementById('popupContainer');
+const hoverDiv = document.querySelector('.hover-div');
+  const targetDiv = document.querySelector('.target-div');
 
-// Get all the popup trigger buttons
-const popupTriggers = document.querySelectorAll('.popup-trigger');
-
-// Add click event listeners to the buttons
-popupTriggers.forEach((button) => {
-  button.addEventListener('click', () => {
-    // Display the popup container
-    popupContainer.classList.add('show');
-
-    // Load and inject the form-pricing.html content into the popup container
-    fetch('/_includes/form-pricing.html')
-      .then((response) => response.text())
-      .then((html) => {
-        popupContainer.innerHTML = html;
-      })
-      .catch((error) => {
-        console.error('Error loading form-pricing.html:', error);
-      });
+  hoverDiv.addEventListener('mouseover', () => {
+    targetDiv.style.display = 'block';
   });
-});
 
-// Function to hide the popup container when clicked outside
-function hidePopupContainer(event) {
-  if (event.target === popupContainer) {
-    popupContainer.classList.remove('show');
-    popupContainer.innerHTML = ''; // Clear the content
-  }
-}
-
-// Add a click event listener to the popup container to hide it when clicked outside
-popupContainer.addEventListener('click', hidePopupContainer);
+  hoverDiv.addEventListener('mouseout', () => {
+    targetDiv.style.display = 'none';
+  });
