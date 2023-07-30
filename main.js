e5dc128eb465451d280759201894c432d7b5f209
brittwoodard <96916819+brittwoodard@users.noreply.github.com>
2023-07-30 12:14:12 +0200

    click to pop up

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-import 'alpinejs';
+console.log("mainjsStart")
+
+const pricingButton = document.querySelectorAll(".pricing-button")
+
+const pricingSection = document.querySelector(".pricing-section")
+
+function togglePricingSection(){
+    console.log("Dan was here.")
+    pricingSection.classList.toggle("active")
+}
+pricingButton.forEach(button=>{
+    button.addEventListener("click",togglePricingSection)
+})
