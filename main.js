a864b40071c579da4f89fe3a4f864f03ed56bfc9
brittwoodard <96916819+brittwoodard@users.noreply.github.com>
2023-09-14 09:54:20 +0200

    jquery

--- main.js
+++ main.js
@@ -73,9 +73,9 @@ $('.slider-container').slick({
 	autoplay: true, // Do we want it to autoplay? true or false
 	autoplaySpeed: 2000, // How long between each slide when auto-playing
 	speed: 500, // How fast is the transition in milliseconds
-	arrows: true, // Do you want to show arrows to trigger each slide
+	arrows: false, // Do you want to show arrows to trigger each slide
 	accessibility: true, // Enables keyboard tabbing and arrow key navigation
-	dots: true, // Enables the dots below to show how many slides
+	dots: false, // Enables the dots below to show how many slides
 	fade: false, // Changes the animate from slide to fade if true
 	infinite: false, // When true, means that it will scroll in a circle
 	pauseOnHover: false, // When true means the autoplay pauses when hovering
