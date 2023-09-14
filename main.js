7de7a63fd00830890ef36ab11f447115f12df0ab
brittwoodard <96916819+brittwoodard@users.noreply.github.com>
2023-09-14 14:15:45 +0200

    Slick

--- main.js
+++ main.js
@@ -70,14 +70,13 @@ document.querySelector("#close-cookie-banner").addEventListener("click", (e) => 
 
 // Slick
 $('.slider-container').slick({
-	autoplay: true, // Do we want it to autoplay? true or false
-	autoplaySpeed: 2000, // How long between each slide when auto-playing
-	speed: 500, // How fast is the transition in milliseconds
-	arrows: true, // Do you want to show arrows to trigger each slide
-	accessibility: true, // Enables keyboard tabbing and arrow key navigation
-	dots: true, // Enables the dots below to show how many slides
-	fade: false, // Changes the animate from slide to fade if true
-	infinite: false, // When true, means that it will scroll in a circle
-	pauseOnHover: false, // When true means the autoplay pauses when hovering
-	pauseOnDotsHover: true, // Pauses the autoplay when hovering over the dots
+	autoplay: true, 
+	autoplaySpeed: 2000, 
+	speed: 500, 
+	arrows: false, 
+	accessibility: true, 
+	dots: false, 
+	fade: true, 
+	infinite: true, 
+	pauseOnHover: false, 
 });
