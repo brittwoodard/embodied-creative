f75af1b3a386a47e6aa3edce618eb778eb76489b
brittwoodard <96916819+brittwoodard@users.noreply.github.com>
2023-10-26 19:49:15 +0200

    moving layouts around for the menu

--- main.js
+++ main.js
@@ -79,12 +79,23 @@ $('.slider-container').slick({
 	pauseOnHover: false, 
 });
 
-// Mbile Menu
+// White Mobile Menu
 // A $( document ).ready() block.
 $(document).ready(function () {
     $(document).ready(function () {
       $(".mobile-button").click(function () {
         $(".mobile-button").toggleClass("active");
+        $(".mobile-menu").toggleClass("active");
+      });
+    });
+  });
+
+  // Dark Mobile Menu
+// A $( document ).ready() block.
+$(document).ready(function () {
+    $(document).ready(function () {
+      $(".mobile-button-dark").click(function () {
+        $(".mobile-button-dark").toggleClass("active");
         $(".mobile-menu").toggleClass("active");
       });
     });
